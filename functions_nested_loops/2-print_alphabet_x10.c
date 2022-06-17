bad0fc5f969d878ea390075055799b59ddf8b062
AvelinoC5 <avelino_63@hotmail.com>
2022-06-17 10:36:17 -0500

    2 file functions

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Write a function that prints 10 times the alphabet, in lowercase,
+ * by new line
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
