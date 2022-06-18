fb3e9fe6fafcb36c2203dc6672e9d617ebceae6a
AvelinoC5 <avelino_63@hotmail.com>
2022-06-17 20:57:39 -0500

    6 file functions

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @r: int r
+ * Return: Success
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+
+	{
+		return (r);
+	}
+
+	else
+	{
+		r *= -1;
+		return (r);
+	}
+}
