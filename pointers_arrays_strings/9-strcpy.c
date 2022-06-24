913d61fb3304ba450bf291f682999f0315d9bded
AvelinoC5 <avelino_63@hotmail.com>
2022-06-23 20:48:39 -0500

    9 file pointers

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - function that copies the string pointed to by sr.
+ * @dest: Pointer char dest
+ * @src: Pointer char src
+ * Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+
+}
