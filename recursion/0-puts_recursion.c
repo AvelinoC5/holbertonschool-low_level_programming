c0159926914a031ce7d0ea280cd69301399bc3a2
AvelinoC5 <avelino_63@hotmail.com>
2022-07-01 20:10:42 -0500

    0 file recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed a new line.
+ * @s: pointer char s
+ */
+
+void _puts_recursion(char *s)
+{
+	if (!s || !*s)
+	{
+		_putchar('\n');
+			return;
+	}
+	_putchar (*s);
+
+	_puts_recursion(s + 1);
+}
