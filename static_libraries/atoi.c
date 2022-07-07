9b5d67aa5ab5777a52406d6d22f1c3d803b38079
AvelinoC5 <avelino_63@hotmail.com>
2022-07-07 10:09:50 -0500

    file create

---
+++ static_libraries/atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - find the numbers in array
+ * @s: pointer to char array input
+ * Return: number find or (0)
+ */
+int _atoi(char *s)
+{
+
+}
---
+++ static_libraries/isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character
+ * @c: is a type int character
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+return (0);
+}
