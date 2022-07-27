db77c6fac15a487904148652d4433e9d630ba5a4
AvelinoC5 <avelino_63@hotmail.com>
2022-07-26 20:23:07 -0500

    file 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - funcion that print all the elementsof a list_t list
+ * @h: pointer to list_t elements
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
