6ce1ac75adf957901d1ecc6587573c063780ad8b
AvelinoC5 <avelino_63@hotmail.com>
2022-07-29 19:12:31 -0500

    file 0 more_lists

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *print_listint - print elements of list
+ *@h: header pointer of the list
+ *Return: elementes of the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
