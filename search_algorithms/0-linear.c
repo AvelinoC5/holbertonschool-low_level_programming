6ce505841de7ba77ed85fcf0e1d35ad895de4451
AvelinoC5 <avelino_63@hotmail.com>
2022-09-08 20:06:14 -0500

    update 0 task

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
 * @size: size of the array
 * @value: matching value to index if found
 * Description: Traverse the array input and match each element to value input
-* Return: -1 if not found or array is NULL OR index if 
+* Return: -1 if not found or array is NULL OR index if
 */
 
 int linear_search(int *array, size_t size, int value)
