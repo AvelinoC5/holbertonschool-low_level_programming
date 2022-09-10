ba0190c10c5310767cdd4036949348f7285c7fc9
AvelinoC5 <avelino_63@hotmail.com>
2022-09-09 19:18:23 -0500

    update 0 task

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,23 +11,25 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
-    int value_indx = 0;
+	size_t i = 0;
+	int value_indx = 0;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (i < size)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            value_indx = i;
-            break;
-        }
-        else
-            value_indx = -1;
-        i++;
-    }
-    return (value_indx);
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			value_indx = i;
+			break;
+		}
+		else
+		{
+			value_indx = -1;
+		}
+		i++;
+	}
+	return (value_indx);
 }
